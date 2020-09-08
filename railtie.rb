7339b7554be3766b98f5d9e10520e6a1d5681616
michel popolin <mulequedoido@gmail.com>
2020-09-08 13:58:17 -0300

    first

--- railtie.rb
+++
@@ -1,8 +0,0 @@
-module Idecoder
-  class Railtie < Rails::Railtie
-    initializer "idecoder" do |app|
-      ActionController::Base.send include: Idecoder::ScriptInserter
-      ActionController::Base.after_filter :insert_idecoder
-    end
-  end
-end
